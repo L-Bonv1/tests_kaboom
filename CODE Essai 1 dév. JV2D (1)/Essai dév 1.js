c1cec64d82472f71606ccd2ab67b556c6382c6df
L-Bonv1 <127302201+L-Bonv1@users.noreply.github.com>
2023-05-03 09:07:00 +0200

    Ajout barrière + dialogue

--- CODE Essai 1 dév. JV2D (1)/Essai dév 1.js
+++ CODE Essai 1 dév. JV2D (1)/Essai dév 1.js
@@ -86,45 +86,43 @@ scene("transition", () => {
 });
 
 //Niveau 1 (interraction 1v1)
+	//Ajout murs infranchissables
+	const level = addLevel([
+  "xxxxxxxxxxxxxxxxx",
+  "x               x",
+  "x               x",
+  "x               x",
+  "x               x",
+  "x               x",
+  "xxxxxxxxxxxxxxxxx",
+], {
+  width: 16,
+  height: 16,
+  "=": [sprite("wall"), area(), body({ isStatic: true })],
+});
+
 scene("Niveau 1", () => {
-	add([
-		sprite("Niveau 1"),
-		pos(width() / 2, height() / 2),
-		anchor("center"),
-	]);
+  add([
+    sprite("Niveau 1"),
+    pos(width() / 2, height() / 2),
+    anchor("center"),
+  ]);
 
-		//Ajout murs infranchissables
-		const level = addLevel([
-			"xxxxxxxxxxxxxxxxx",
-			"x               x",
-			"xxxxxxxxxxxxxxxxx",
-		],{
-			tileWidth: 16,
-			tileHeight: 16,
-			tiles: {
-				"x": () => [
-					sprite("wall_left"),
-					area({ shape: new Rect(vec2(0), 4, 16) }),
-					body({ isStatic: true }),
-					tile({ isObstacle: true }),
-				]},
-			})
-	
-	const player = add([
-		sprite("hero"),
-		pos(450, 300),
-		scale(2),
-		area({ width: 2, height: 2}),
-		body(),
-	]);
+  const player = add([
+    sprite("hero"),
+    pos(450, 300),
+    scale(2),
+    area({ width: 2, height: 2 }),
+    body(),
+  ]);
 
-	const Ogre = add([
-		sprite("ogre"),
-		pos(250, 120),
-		scale(2),
-		area({ width: 2, height: 2}),
-		body({isStatic: true}),	
-	]);
+  const Ogre = add([
+    sprite("ogre"),
+    pos(250, 120),
+    scale(2),
+    area({ width: 2, height: 2 }),
+    body({ isStatic: true }),
+  ]);
 	
 	// Vitesse de déplacement du joueur
 	  const SPEED = 140;
@@ -150,6 +148,21 @@ scene("Niveau 1", () => {
 	  });
 	
 	});
+
+	//Dialogue entre Player et Ogre lorsqu'ils entrent en collision
+	player.collides("ogre", () => {
+		add([
+		  text("Ogre: Hey toi, qui es-tu ?", 10),
+		  pos(width() / 2, height() / 3),
+		  origin("center"),
+		]);
+	
+		add([
+		  text("Joueur: Je suis un aventurier !", 10),
+		  pos(width() / 2, height() / 2),
+		  origin("center"),
+		]);
+	  });
 	
 	// Démarrez le jeu sur l'écran d'accueil
 	go("home");
